f092feac80bbb3a6861678edce4094066809bc7c
aoi yodono <simasimano01@gmail.com>
2019-11-13 12:42:44 +0900

    mail調整

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', ''),
+        'address' => env('MAIL_FROM_ADDRESS', 'simasimano01@gmail.com'),
         'name' => env('MAIL_FROM_NAME', ''),
     ],
 
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME', 'simasimano01@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD', 'oekjcnbjwsbcuvtm'),
 
     /*
     |--------------------------------------------------------------------------
